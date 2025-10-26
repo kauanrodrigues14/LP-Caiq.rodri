e174c04c1c7b805d9765414772a98a26d243af2b
kauanrodrigues14 <kauanoddy@gmail.com>
2025-10-26 10:29:42 -0300

    services and home animations

--- js/script.js
+++ js/script.js
@@ -1,27 +1,39 @@
-/*const sections = document.querySelectorAll('section')
-
-const MyObserver = new IntersectionObserver((entries) => {
-    entries.forEach(entry => {
-        if (entry.isIntersecting) {
-            entry.target.classList.add('show')
-        }
-        else {
-            entry.target.classList.remove('show')
-        }
-    }   )
-}
-, {})  */  
-
 function sendMessage(event){
 
     event.preventDefault()
 
     const name = document.getElementById('name').value
     const message = document.getElementById('message').value
-    const cellphone = '11231231231231'
+    const cellphone = '11989190286'
     const text = `Olá, meu nome é ${name} , ${message}`
     const url = `https://api.whatsapp.com/send?phone=${cellphone}&text=${encodeURIComponent(text)}`
     window.open(url, '_blank').focus()
 
 
-}
+};     
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const boxes = document.querySelectorAll('.services-box');
+
+  const options = {
+    root: null,
+    threshold: 0.4
+  };
+
+  const myObserver = new IntersectionObserver((entries) => {
+    entries.forEach((entry, delay) => {
+      if (entry.isIntersecting) {
+        entry.target.style.animationDelay = `${delay * 0.3}s`;
+        entry.target.classList.add('show');
+        console.log('Visível:', entry.target);
+        
+       
+      } else {
+        entry.target.classList.remove('show');
+      }
+    });
+  }, options);
+
+  boxes.forEach((div) => myObserver.observe(div));
+});
